c351e722b540fe9ba911e4684f3cfa1828bde784
Dubster <shevc9@mail.ru>
2023-09-27 16:09:04 +0300

    fixed controllable jump

--- Assets/Scripts/WallJump.cs
+++ Assets/Scripts/WallJump.cs
@@ -7,8 +7,9 @@ public class WalljumpMove : BasicMoveScript
     void FixedUpdate()
     {
         SurfaceCheck(Vector2.down, col.bounds.extents.y + 0.1f, out isGround, ground);
-        SurfaceCheck(Vector2.right, col.bounds.extents.x + 0.1f, out isWall, wall);
-        SurfaceCheck(Vector2.left, col.bounds.extents.x + 0.1f, out isWall, wall);
+        SurfaceCheck(Vector2.right, col.bounds.extents.x + 0.1f, out isRightWall, wall);
+        SurfaceCheck(Vector2.left, col.bounds.extents.x + 0.1f, out isLeftWall, wall);
+        isWall = isRightWall || isLeftWall;
         Walljump();
         Jump();
         FallAcceleration(true);
@@ -16,6 +17,8 @@ public class WalljumpMove : BasicMoveScript
     }
 
     public float blockDuration = 0.5f;
+
+    private bool isLeftWall, isRightWall;
 
     private float blockTime = 0;
     void Walljump()
@@ -27,8 +30,17 @@ public class WalljumpMove : BasicMoveScript
             if (!isGround)
                 if (Input.GetKeyDown(KeyCode.Space))
                 {
-                    body.velocity = new Vector2(Mathf.Cos(Mathf.PI / 3), Mathf.Sin(Mathf.PI / 6));
-                    body.AddForce(new Vector2(2 * Mathf.Cos(Mathf.PI / 3), 2 * Mathf.Sin(Mathf.PI / 6)) * jumpForce);
+                    if (isLeftWall)
+                    {
+                        body.velocity = new Vector2(Mathf.Cos(Mathf.PI / 3), Mathf.Sin(Mathf.PI / 6));
+                        body.AddForce(new Vector2(2 * Mathf.Cos(Mathf.PI / 3), 2 * Mathf.Sin(Mathf.PI / 6)) * jumpForce);
+                    }
+                    else
+                    {
+                        body.velocity = new Vector2(Mathf.Cos(Mathf.PI / 3) * -1, Mathf.Sin(Mathf.PI / 6));
+                        body.AddForce(new Vector2(-2 * Mathf.Cos(Mathf.PI / 3), 2 * Mathf.Sin(Mathf.PI / 6)) * jumpForce);
+                    }
+                    
                     blockX = true;
                 }
             SurfaceCheck(Vector2.down, col.bounds.extents.y + 0.1f, out isGround, ground);
