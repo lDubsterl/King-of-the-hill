f650dada99b4f8675fd66f14b61835e39f646e02
Dubster <shevc9@mail.ru>
2023-10-05 18:30:08 +0300

    Added boosts and player spawners, local multiplayer(by Tvarb1na)

---
+++ Assets/Scripts/DestroyBoost.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBoost : MonoBehaviour
+{
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) // если другой объект имеет тег "Enemy"
+        {
+            Destroy(this.gameObject); // уничтожаем другой объект
+            BoostSpawner._isBoostExist = false;
+        }
+    }
+}
